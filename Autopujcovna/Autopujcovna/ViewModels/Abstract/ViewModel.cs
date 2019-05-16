490e0ec58b27cf70b236c363303e14829f970db3
pavelloubek <pavel.loubek@pslib.cz>
2019-05-16 14:05:23 +0200

    Added every file

---
+++ Autopujcovna/Autopujcovna/ViewModels/Abstract/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Autopujcovna.ViewModels.Abstract
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Autopujcovna/Autopujcovna/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autopujcovna.Views;
+using Xamarin.Forms;
+
+namespace Autopujcovna.ViewModels
+{
+    class MainViewModel : Abstract.ViewModel
+    {
+        public MainViewModel()
+        {
+            UkazSeznam = new Command(UkazSeznam_e);
+            UkazPridat = new Command(UkazPridat_e);
+            UkazOdebrat = new Command(UkazOdebrat_e);
+        }
+
+        public Command UkazSeznam { get; private set; }
+        public Command UkazPridat { get; private set; }
+        public Command UkazOdebrat { get; private set; }
+
+        void UkazSeznam_e(object sender)
+        {
+            App.Current.MainPage = new NavigationPage(new SeznamView());
+        }
+
+        void UkazPridat_e(object sender)
+        {
+            App.Current.MainPage = new NavigationPage(new PridatView());
+        }
+
+        void UkazOdebrat_e(object sender)
+        {
+            App.Current.MainPage = new NavigationPage(new OdebratView());
+        }
+    }
+}

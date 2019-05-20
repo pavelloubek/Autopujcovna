4d5cb55dcc0ebfc3974be880687c0920caf46aa4
pavelloubek <pavel.loubek@pslib.cz>
2019-05-20 21:17:38 +0200

    Added OdebratView

---
+++ Autopujcovna/Autopujcovna/ViewModels/OdebratViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Autopujcovna.ViewModels
+{
+    class OdebratViewModel
+    {
+    }
+}
--- Autopujcovna/Autopujcovna/obj/Debug/netstandard2.0/Views/OdebratView.xaml.g.cs
+++ Autopujcovna/Autopujcovna/obj/Debug/netstandard2.0/Views/OdebratView.xaml.g.cs
@@ -17,8 +17,12 @@ namespace Autopujcovna.Views {
     public partial class OdebratView : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Button PotvrditOdebrat;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(OdebratView));
+            PotvrditOdebrat = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "PotvrditOdebrat");
         }
     }
 }

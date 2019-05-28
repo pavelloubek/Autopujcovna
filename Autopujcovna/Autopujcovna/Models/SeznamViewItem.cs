efdc1e47bd0b3f3e3a32b86e4766ff67ce429cd3
pavelloubek <pavel.loubek@pslib.cz>
2019-05-28 10:56:56 +0200

    Moved SeznamViewItem from ViewModelItem to Models

--- Autopujcovna/Autopujcovna/Models/SeznamViewItem.cs
+++ Autopujcovna/Autopujcovna/Models/SeznamViewItem.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Autopujcovna.ViewModels
+namespace Autopujcovna.Models
 {
-    class SeznamViewModel
+    class SeznamViewItem
     {
-
     }
 }
